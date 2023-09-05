72ab326e327d78615ffeb1fa5f7b225835719c0b
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-09-05 16:53:07 -0300

    move db connection

--- src/infra/db.ts
+++ src/infra/db.ts
@@ -3,9 +3,7 @@ import 'dotenv/config'
 
 export async function connect() {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yq0he2b.mongodb.net/users`
-    )
+    await mongoose.connect(process.env.MONGODB_CONNECT_URI || '')
   } catch (error) {
     console.log(error)
   }
